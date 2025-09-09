a3819e85c7e6e727272580983b87876ca4efc497
MahmoodEbrahim <m7kmod7k2003@gmail.com>
2025-09-09 19:14:31 +0300

    add start_screen.dart

--- lib/features/start_screen/presentation/start_screen.dart
+++ lib/features/start_screen/presentation/start_screen.dart
@@ -10,74 +10,73 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth  = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: screenWidth*0.04,vertical:screenHeight*0.03 ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-             SizedBox(height: screenHeight* 0.02),
-            const Text(
-              "Welcome to Sportiva",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-             SizedBox(height: screenHeight* 0.01),
-            const Text(
-              "Please login to your account or create new account to continue",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-            ),
-            Spacer(),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const LoginScreen()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                minimumSize: const Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth*0.04,vertical:screenHeight*0.03 ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               SizedBox(height: screenHeight* 0.02),
+              const Text(
+                "Welcome to Sportiva",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              child: const Text(
-                "LOGIN",
-                style: TextStyle(fontSize: 16, color: Colors.white),
-              ),
-            ),
-             SizedBox(height: screenHeight*0.01),
-            OutlinedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const SignupScreen()),
-                );
-              },
-              style: OutlinedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-                side: const BorderSide(color: Colors.green),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+               SizedBox(height: screenHeight* 0.02),
+              const Text(
+                "Please login to your account or create new account to continue",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
                 ),
               ),
-              child: const Text(
-                "CREATE ACCOUNT",
-                style: TextStyle(fontSize: 16, color: Colors.green),
+              SizedBox(height: screenHeight*0.25,),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const LoginScreen()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  "LOGIN",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
               ),
-            ),
-          ],
+               SizedBox(height: screenHeight*0.02),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const SignupScreen()),
+                  );
+                },
+                style: OutlinedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                  side: const BorderSide(color: Colors.green),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  "CREATE ACCOUNT",
+                  style: TextStyle(fontSize: 16, color: Colors.green),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
